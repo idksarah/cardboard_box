5980992cdcd3e44336a7d88a3d37fc0f4e868a77
idksarah <sarah.p.ngai@gmail.com>
2025-07-13 21:55:49 -0700

    idk tbf

--- main.py
+++ main.py
@@ -1,40 +1,64 @@
-#This tutorial is provided by TomoDesign / https://www.instagram.com/tomo_designs/ 
 import time
 import board
 import digitalio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
+from digitalio import DigitalInOut, Direction, Pull
 
 print("hello world")
 
+# led = DigitalInOut(board.GP3)
+# led.direction = Direction.OUTPUT
+
 kbd = Keyboard(usb_hid.devices)
 
-# define buttons. these can be any physical switches/buttons, but the values
-button1 = digitalio.DigitalInOut(board.GP1)
-button1.direction = digitalio.Direction.INPUT
-button1.pull = digitalio.Pull.UP
+def setup_button(pin):
+    btn = digitalio.DigitalInOut(pin)
+    btn.direction = digitalio.Direction.INPUT
+    btn.pull = digitalio.Pull.UP
+    return btn
 
-button2 = digitalio.DigitalInOut(board.GP2)
-button2.direction = digitalio.Direction.INPUT
-button2.pull = digitalio.Pull.UP
+def setup_cherry(pin):
+    btn = digitalio.DigitalInOut(pin)
+    btn.direction = digitalio.Direction.INPUT
+    btn.pull = digitalio.Pull.UP
+    return btn
 
-button15 = digitalio.DigitalInOut(board.GP15)
-button15.direction = digitalio.Direction.INPUT
-button15.pull = digitalio.Pull.UP
+button1 = setup_button(board.GP3)
+button2 = setup_button(board.GP2)
+button15 = setup_button(board.GP1)
+button16 = setup_button(board.GP16)
+enter = setup_cherry(board.GP7)
+escape = setup_cherry(board.GP9)
 
-button16 = digitalio.DigitalInOut(board.GP16)
-button16.direction = digitalio.Direction.INPUT
-button16.pull = digitalio.Pull.UP
+# Button states: True means unpressed, False means pressed
+prev_states = {
+    button1: True,
+    button2: True,
+    button15: True,
+    button16: True
+}
 
-while True:
-    if button1.value:
-        kbd.send(Keycode.W)
-    if button2.value:
-        kbd.send(Keycode.D)
-    if button15.value:
-        kbd.send(Keycode.A)
-    if button16.value:
-        kbd.send(Keycode.S)
+button_keys = {
+    button1: Keycode.D,
+    button2: Keycode.S,
+    button15: Keycode.W,
+    button16: Keycode.A
+}
 
-    time.sleep(0.08)
+while True:    
+#     led.value = False
+#     print (button1.vaaaaaalue)
+    if not enter.value:
+        kbd.send(Keycode.ENTER)
+    if not escape.value:
+        kbd.send(Keycode.ESCAPE)
+    for btn, prev in prev_states.items():
+        current = btn.value
+        if prev and not current:  # just pressed
+            kbd.release(button_keys[btn])
+        elif not prev and current:  # just released
+            kbd.press(button_keys[btn])
+        prev_states[btn] = current
+    time.sleep(0.01)
